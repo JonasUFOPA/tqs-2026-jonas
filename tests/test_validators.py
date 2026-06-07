a530db3109ae1fc7733b3f40b946d24e90edc273
JonasUFOPA <102568230+JonasUFOPA@users.noreply.github.com>
2026-06-06 23:37:48 -0300

    Feat/aceitar espacos nas pontas (#3)
    * test: adiciona testes para CPF/email com espaços nas pontas
    
    * feat: aceita CPF e email com espaços nas pontas
    
    * chore: espelha trim no validators.js

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -27,21 +27,9 @@ def test_rejeita_email_sem_arroba():
     assert validar_email("semarroba.com") is False
 
 
-def test_aceita_cnpj_valido_com_mascara():
-    assert validar_cnpj("11.222.333/0001-81") is True
+def test_aceita_cpf_com_espacos_nas_pontas():
+    assert validar_cpf("  111.444.777-35  ") is True
 
 
-def test_aceita_cnpj_valido_sem_mascara():
-    assert validar_cnpj("11222333000181") is True
-
-
-def test_rejeita_cnpj_com_digito_verificador_errado():
-    assert validar_cnpj("11.222.333/0001-82") is False
-
-
-def test_rejeita_cnpj_com_todos_digitos_iguais():
-    assert validar_cnpj("11111111111111") is False
-
-
-def test_rejeita_cnpj_vazio():
-    assert validar_cnpj("") is False
+def test_aceita_email_com_espacos_nas_pontas():
+    assert validar_email("  aluno@ufopa.edu.br  ") is True
